90373255ef4aadd262d634340295a6875fbb9b73
jmorrisosu@gmail.com <jmorrisosu@gmail.com@77a4fc73-e944-9af1-8b44-5fee765067de>
2012-01-31 01:34:29 +0000

    Background image, added no functionality paddle, improved ball mechanics

--- branches/MvcApplication1/MvcApplication1/Scripts/main.js
+++ branches/MvcApplication1/MvcApplication1/Scripts/main.js
@@ -3,17 +3,26 @@ var ctx = c.getContext("2d");
 
 var FPS = 30;
 
+//Background Image
+var img = new Image();
+img.src = "../Content/funnypictures_91.jpg";
+
 //Ball coordinates
 var BallX = 110;
 var BallY = 42;
+
+//Paddle coordinates
+var PaddleX = 200;
+var PaddleY = 390;
+var PaddleWidth = 75;
 
 //Canvas size, still needs changed when you update canvas tag, would be nice to possibly pull in values here.
 var CanvasWidth = 400;
 var CanvasHeight = 400;
 
 //Ball Velocity
-var SpeedX = 15;
-var SpeedY = 15;
+var SpeedX = 10;
+var SpeedY = 10;
 
 setInterval(function () {
     update();
@@ -40,14 +49,24 @@ function draw() {
     //Clear Screen
     ctx.clearRect(0, 0, CanvasWidth, CanvasHeight);
 
+    //Draw background img
+    ctx.drawImage(img, 0, 0);
+
     //Item Color
     ctx.fillStyle = "#FF0000";
 
     //Draw ball
     ctx.beginPath();
-    ctx.arc(BallX, BallY, 10, 0, Math.PI * 2, true);
+    ctx.arc(BallX, BallY, 8, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.fill();
     //End ball
+
+    //Draw Paddle
+    ctx.beginPath();
+    ctx.moveTo(PaddleX-(PaddleWidth/2), PaddleY);
+    ctx.lineTo(PaddleX+(PaddleWidth/2), PaddleY);
+    ctx.stroke();
+    //End Paddle
 }
 
