23f29aee56b7d24a6668871e278134efe27bf7cf
jmorrisosu@gmail.com <jmorrisosu@gmail.com@77a4fc73-e944-9af1-8b44-5fee765067de>
2012-02-07 15:10:12 +0000

    --Moved scripts into their own folder to keep them from getting tangled into jquery. --Moved all sprites to Sprites.js --Moved all input control to Input.js

---
+++ branches/MvcApplication1/MvcApplication1/GameScripts/Sprites.js
@@ -0,0 +1,32 @@
+﻿//Ball coordinates
+var Ball = {
+    X: 200,
+    Y: 351,
+    Radius: 6,
+    SpeedX: -7,
+    SpeedY: -8
+};
+
+//Paddle coordinates
+var Paddle = {
+    X: 200,
+    Y: 365,
+    Width: 85,
+    Speed: 12
+};
+
+//Block
+var Block = {
+    a: {
+        id: "purple",
+        health: 1,
+        color: "#512555",
+        broken: false
+    },
+    z: {
+        id: "empty",
+        health: 0,
+        color: null,
+        broken: true
+    }
+};
