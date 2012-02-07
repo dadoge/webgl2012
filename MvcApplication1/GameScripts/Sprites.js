bb73455f8e6a0f6b143634c93e861b73cc083fa7
jmorrisosu <jmorrisosu@gmail.com>
2012-02-07 23:57:24 +0000

    Updated location of scripts, refactored location of much code related to blocks to hopefully better organize this and allow us to clean up our code.
    Would like to also make a gamestate class and js file where this data is stored.

--- MvcApplication1/GameScripts/Sprites.js
+++ MvcApplication1/GameScripts/Sprites.js
@@ -30,3 +30,141 @@ var Block = {
         broken: true
     }
 };
+
+/////////////////////////////////////////////////////////////////////
+//                        Block Creation
+/////////////////////////////////////////////////////////////////////
+
+function createBlock(extX, extY, extHealth, extColor, extBroken) {
+    var block;
+    block = { health: extHealth, x: extX, y: extY, color: extColor, broken: extBroken };
+    //var health = extHealth;
+    //var x = extX;
+    //var y = extY;
+    //var color = extColor;
+    //var broken = extBroken;
+    return block;
+}
+
+function setupLevel() {
+    for (var i = 0; i < level.length; i++) {
+        for (var j = 0; j < level[i].length; j++) {
+            if (level[i][j] == "purple") {
+                level[i][j] = createBlock(j, i, 1, "#660099", false);
+                numBlocks++;
+            }
+            else if (level[i][j] == "orange") {
+                level[i][j] = createBlock(j, i, 1, "#FF9900", false);
+                numBlocks++;
+            }
+            else if (level[i][j] == "empty") {
+                level[i][j] = null;
+            }
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+//                                  Brick collision
+//////////////////////////////////////////////////////////////////////////////////////
+function collisionXWithBlocks() {
+    var bumpedX = false;
+    var currBlock;
+    for (var i = 0; i < level.length; i++) {
+        for (var j = 0; j < level[i].length; j++) {
+            currBlock = level[i][j];
+
+            if (currBlock != null) {
+                if (currBlock.health > 0) { // if brick is still visible
+                    var blockX = j * blockWidth;
+                    var blockY = i * blockHeight;
+                    if (
+                    // barely touching from left
+                    ((Ball.X + Ball.SpeedX + Ball.Radius >= blockX) &&
+                    (Ball.X + Ball.Radius <= blockX))
+                    ||
+                    // barely touching from right
+                    ((Ball.X + Ball.SpeedX - Ball.Radius <= blockX + blockWidth) &&
+                    (Ball.X - Ball.Radius >= blockX + blockWidth))
+                    ) {
+                        if ((Ball.Y + Ball.SpeedY - Ball.Radius <= blockY + blockHeight) &&
+                        (Ball.Y + Ball.SpeedY + Ball.Radius >= blockY)) {
+                            // weaken block and increase score
+                            explodeBlock(i, j);
+                            //breakBlock(currBlock);
+
+                            bumpedX = true;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+    return bumpedX;
+}
+
+function collisionYWithBlocks() {
+    var bumpedY = false;
+    var currBlock;
+    for (var i = 0; i < level.length; i++) {
+        for (var j = 0; j < level[i].length; j++) {
+            currBlock = level[i][j];
+            if (currBlock != null) {
+                if (currBlock.health > 0) { // if brick is still visible
+                    var blockX = j * blockWidth;
+                    var blockY = i * blockHeight;
+                    if (
+                    // barely touching from below
+                    ((Ball.Y + Ball.SpeedY - Ball.Radius <= blockY + blockHeight) &&
+                    (Ball.Y - Ball.Radius >= blockY + blockHeight))
+                    ||
+                    // barely touching from above
+                    ((Ball.Y + Ball.SpeedY + Ball.Radius >= blockY) &&
+                    (Ball.Y + Ball.Radius <= blockY))) {
+                        if (Ball.X + Ball.SpeedX + Ball.Radius >= blockX &&
+                        Ball.X + Ball.SpeedX - Ball.Radius <= blockX + blockWidth) {
+                            // weaken block and increase score
+                            explodeBlock(i, j);
+                            //breakBlock(currBlock);
+
+                            bumpedY = true;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+    return bumpedY;
+}
+
+function breakBlock(block) {
+    var currBlock = block;
+    currBlock.health--;
+
+    if (currBlock.health > 0) {
+        GameScore++;
+    }
+    else {
+        GameScore += 2;
+        currBlock.broken = true;
+    }
+}
+
+function explodeBlock(i, j) {
+    // First weaken the block (0 means block is gone)
+    var currBlock;
+    currBlock = level[i][j];
+    currBlock.health--;
+
+    if (currBlock.health > 0) {
+        // The block is weakened but still around. Give a single point.
+        GameScore++;
+    } else {
+        // give player an extra point when the block disappears
+        GameScore += 2;
+        currBlock.broken = true;
+        numBlocks--;
+    }
+}
