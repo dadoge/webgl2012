fb82b68998fc9fffc5167a13f2e7b41881f0a51f
jmorrisosu <jmorrisosu@gmail.com>
2013-02-19 01:48:06 +0000

    Merged code from cloud 9

--- game.js
+++ game.js
@@ -6,19 +6,19 @@ ctx.fillStyle="#333333";
 
 var CanvasWidth = Canvas.width;
 var CanvasHeight = Canvas.height;
+var count=0;
 
 var heroImage = new Image();
-heroImage.src = 'spriterght2.jpg';
+heroImage.src = 'Ot_FrontSprite_1.png';
 
     var hero = {
         Image: heroImage,
         X: 20,
         Y: 30,
         draw : function(){
-            ctx.drawImage(this.Image,this.X,this.Y)
+            ctx.drawImage(this.Image,this.X,this.Y);
         }
     };
-
 startGame();
 
 function startGame() {
@@ -27,20 +27,64 @@ function startGame() {
 
 function gameLoop(){
     InitFloor();
-    hero.draw();
     processInput();
+    hero.draw();
+    
+    
+    //Things that have to do with processing input or conditionals based upon input parameters need to be in processinput() or a function it calls
+//    if(DownDown && !UpDown)
+//    {
+//        //if you never change step whats the point?
+//        var step=3;
+//        
+//        //step * 1 means nothings
+//        if (count<step*1){
+//            //Please only update the hero object, not the hero image
+//            heroImage.src = 'Ot_FrontSprite_1.png';
+//            
+//            //replace your N hero.draws() with a single hero.draw()
+//            hero.draw();
+//        }
+//        else if(count>=step*1 && count<step*2){
+//            //Also lets put all images into string varables
+//            heroImage.src = 'Ot_FrontSprite_2.png';
+//            hero.draw();
+//        }
+//        else if(count>=step*2 && count<step*3){
+//            heroImage.src = 'Ot_FrontSprite_3.png';
+//            hero.draw();
+//        }
+//        else if(count>=step*3 && count<step*4){
+//            heroImage.src = 'Ot_FrontSprite_4.png';
+//            hero.draw();
+//        }
+//        else if(count>=step*4){
+//            heroImage.src = 'Ot_FrontSprite_1.png';
+//            hero.draw();
+//            count=0;
+//        }
+//        count=count+1;
+//    }
+//    else{
+//        heroImage.src = 'Ot_FrontSprite_1.png';
+//        hero.draw();
+//    }
 }
 function InitFloor()
 {
+    ctx.fillStyle="#ffffff";
+    ctx.fillRect(0,0,CanvasWidth,CanvasHeight);
     var tileheight=20;
     var tilewidth=20;
-    var x_numtiles=Math.floor(CanvasHeight/tileheight);
-    var y_numtiles=Math.floor(CanvasWidth/tilewidth);
-    for (var i=0;i<x_numtiles;i++)
+    var x_tiles=Math.floor(CanvasHeight/tileheight);
+    var y_tiles=Math.floor(CanvasWidth/tilewidth);
+    for (var i=0;i<x_tiles;i++)
     {	
-        for (var j=0;j<y_numtiles;j++)
+        ctx.fillStyle="#7E5B52";
+        for (var j=0;j<y_tiles;j++)
         {
             ctx.fillRect(i*tileheight+i,j*tilewidth+j,tileheight,tilewidth);
+            
         }
     }
 }
