e601ee06fe2cbf66a6f6df6868c09a2108bedddd
jmorrisosu@gmail.com <jmorrisosu@gmail.com@77a4fc73-e944-9af1-8b44-5fee765067de>
2013-05-08 02:11:17 +0000

    getting rurlll in hurr

--- branches/RTS.JS/RTS.JS/GameScripts/unit.js
+++ branches/RTS.JS/RTS.JS/GameScripts/unit.js
@@ -1,13 +1,17 @@
-function Unit(type) {
+function Unit(type, sprite) {
     this.type = type;
     this.damage = 5;
     this.health = 20;
-    this.x = 0;
+    this.x = 100;
     this.y = 100;
     this.height = 5;
     this.width = 5;
+    this.image = sprite;
     this.takeDamage = function (damage) {
         this.heath = this.health - damage;
     };
+    this.draw = function (ctx) {
+        ctx.drawImage(this.image, this.x, this.y);
+    }
 }
 
