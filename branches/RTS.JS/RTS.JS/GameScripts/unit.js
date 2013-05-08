1f679c178598669581593c1c8d405c007f143beb
jimcarrey363 <jimcarrey363@gmail.com>
2013-05-08 03:58:14 +0000

    first addition of 2nd character. added 2nd method instead of editing original due to time. flipped image in GIMP

--- branches/RTS.JS/RTS.JS/GameScripts/unit.js
+++ branches/RTS.JS/RTS.JS/GameScripts/unit.js
@@ -26,5 +26,20 @@ function Unit(type, sprite) {
             this.state = 0
         }
     }
+	this.draw2 = function (ctx) {
+        ctx.drawImage(this.image, 0 + this.state, 0, 64, 68, this.x, this.y, 64, 68);
+        this.x -= 5;
+
+        if(this.x % 20 == 0)
+        {
+            this.state += 64;
+        }
+        if (this.x < 0) {
+            this.x = 500;
+        }
+        if (this.state > 192) {
+            this.state = 0
+        }
+    }
 }
 
