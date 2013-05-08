809540219e9ca4ef8ffd9c32d06d1ff1813ea7f6
jmorrisosu <jmorrisosu@gmail.com>
2013-05-08 01:34:13 +0000

    unit code, not working yet tho

--- RTS.JS/GameScripts/main.js
+++ RTS.JS/GameScripts/main.js
@@ -30,7 +30,7 @@ preinit();
 function preinit() {
 
     var newUnit = new Unit("x");
-
+    newUnit.animate(ctx);
     //Draw bottom pit
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, Canvas.Width, Canvas.Height);
