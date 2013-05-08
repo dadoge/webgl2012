d4226cb0bdf85047fbce23fa44282fd8f83e3bba
jmorrisosu <jmorrisosu@gmail.com>
2013-05-08 01:01:32 +0000

    Trimming down all the old stuff to make room for the new

--- branches/RTS.JS/RTS.JS/GameScripts/main.js
+++ branches/RTS.JS/RTS.JS/GameScripts/main.js
@@ -8,8 +8,8 @@ var ctx = c.getContext("2d");
 
 //Canvas size
 var Canvas = {
-    Height: c.width,
-    Width: c.height
+    Height: c.height,
+    Width: c.width
 };
 
 //Game Variables
@@ -17,11 +17,7 @@ var intervalID;
 var FPS = 30;
 var isGameActive = false;
 var isPaused = false;
-
-
-//Background Image
-var backgroundImg = new Image();
-backgroundImg.src = "../Content/incomingbaby.jpg";
+var groundHeight = 50;
 
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -35,13 +31,15 @@ function preinit() {
 
     //Draw bottom pit
     ctx.fillStyle = "#000000";
-    ctx.fillRect(0, 0, Canvas.Height, Canvas.Width);
+    ctx.fillRect(0, 0, Canvas.Width, Canvas.Height);
+
+    ctx.fillStyle = "#00FF00";
+    ctx.fillRect(0, Canvas.Height-groundHeight, Canvas.Width, Canvas.Height);
 
     //Draw Score and lives
     ctx.font = "20pt Arial";
     ctx.textBaseline = "top";
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillText("Oh herro!  Press space to pray!", 10, (Canvas.Height / 2 - 10));
 }
 
 
@@ -68,50 +66,16 @@ function draw() {
     //create level
     createLevel();
 
-    //Draw ball
-    ctx.fillStyle = "#FF0000";
-    ctx.beginPath();
-    ctx.arc(Ball.X, Ball.Y, Ball.Radius, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.fill();
-    //End ball
-
-    //Draw Paddle
-    ctx.strokeStyle = "#888888";
-    ctx.lineCap = "round";
-    ctx.lineWidth = 6;
-    ctx.beginPath();
-    ctx.moveTo(Paddle.X - (Paddle.Width / 2), Paddle.Y);
-    ctx.lineTo(Paddle.X + (Paddle.Width / 2), Paddle.Y);
-    ctx.stroke();
-    ctx.closePath();
-    //End Paddle
-
-    //Draw bottom pit
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(0, 370, 400, 30);
-
-    //Draw Score and lives
+    //Draw Score
     ctx.font = "14pt Arial";
     ctx.textBaseline = "top";
     ctx.fillStyle = "#FFFFFF";
     ctx.fillText("Score: " + GameScore, 10, 370);
-    ctx.fillText("Lives: " + lives, 300, 370);
-    ctx.fillText("Level: " + (currentLevel + 1), 150, 370);
 
-    drawPowerup();
 
     if (isPaused) {
         ctx.fillText("Game Paused. Press 'p' to resume.", 50, 200);
         clearInterval(intervalID);
-    }
-
-    if (gamewon) {
-
-        ctx.font = "20pt Arial";
-        ctx.textBaseline = "top";
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillText("You won you lucky basturd!!" + GameScore, 10, 200);
     }
 }
 
