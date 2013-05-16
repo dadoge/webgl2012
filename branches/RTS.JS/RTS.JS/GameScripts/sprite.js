2ba4fd69535a9105aa2a90fd0ec743ad502afcb0
jmorrisosu@gmail.com <jmorrisosu@gmail.com@77a4fc73-e944-9af1-8b44-5fee765067de>
2013-05-16 02:40:49 +0000

    Forgot to have sprite.js checked in. type.js incoming.

---
+++ branches/RTS.JS/RTS.JS/GameScripts/sprite.js
@@ -0,0 +1,8 @@
+function Sprite(image, frameWidth, frameHeight, xFrames, yFrames) {
+    this.image = image;
+    this.frameWidth = frameWidth;
+    this.frameHeight = frameHeight;
+    this.xFrames = xFrames;
+    this.yFrames = yFrames;
+}
+   
