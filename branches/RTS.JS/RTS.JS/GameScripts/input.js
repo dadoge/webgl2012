2366c347a29a203bcd9bcdc5a6e0fd99ded51651
jmorrisosu <jmorrisosu@gmail.com>
2013-05-16 22:06:03 +0000

    significant modularization improvements to input.js.
    Added way better buttons to the game.

--- branches/RTS.JS/RTS.JS/GameScripts/input.js
+++ branches/RTS.JS/RTS.JS/GameScripts/input.js
@@ -12,85 +12,24 @@ var KEYS = {
 //Key Listeners
 window.addEventListener('keydown', doKeyDown, true);
 
-$('#btnStart').on("click", doStart);
-$('#btnStart2').on("click", doStart2);
-//$('#btnStart').on("touchstart", doStart);
+$('#btn_leftA').on("click", sendLeftUnit);
+$('#btn_rightA').on("click", sendRightUnit);
+
+$('#btn_start').on("click", start);
 ////////////////////////////////////////////////////////////////////////////
 //                        Callbacks
 ///////////////////////////////////////////////////////////////////////////
 
-function doStart() {
-    if (isGameActive == false) {
-        isGameActive = true;
-        startGame();
-    }
-
-    if (leftTeamMoney > leftType.cost) {
-        var newLeftType = {
-            speed: 5,
-            direction: 1,
-            health: 10,
-            damage: 3,
-            cost: 10,
-            team: 'left'
-        };
-        leftTeamUnits.push(new Unit(newLeftType, robotSprite, 64, 68, 4, 0, 400, ++unitCount));
-        leftTeamMoney -= leftType.cost;
-    }
-}
-function doStart2() {
-    if (isGameActive == false) {
-        isGameActive = true;
-        startGame();
-    }
-
-    if (rightTeamMoney >= rightType.cost) {
-        var newRightType = {
-            speed: 5,
-            direction: -1,
-            health: 10,
-            damage: 3,
-            cost: 10,
-            team: 'right'
-        };
-        rightTeamUnits.push(new Unit(newRightType, robotSprite2, 64, 68, 4, Canvas.Width, 400, ++unitCount));
-        rightTeamMoney -= rightType.cost;
-    }
-}
 function doKeyDown(evt) {
     switch (evt.keyCode) {     
         case KEYS.LEFT:
-			if(leftTeamMoney >= leftType.cost)
-			{
-			    var newLeftType = new UnitType(5,1,10,3,10,'left');
-				leftTeamUnits.push(new Unit(newLeftType, blueRobotSprite, 0, 400, ++unitCount));
-				leftTeamMoney -= leftType.cost;
-			}
-            LeftDown = true;
+			sendLeftUnit();
             break;    
         case KEYS.RIGHT:
-			if(rightTeamMoney > rightType.cost)
-			{
-			    var newRightType = new UnitType(5,-1,10,3,10,'right');
-				rightTeamUnits.push(new Unit(newRightType, pinkRobotSprite, Canvas.Width, 400, ++unitCount));
-				rightTeamMoney -= rightType.cost;
-			}
-            RightDown = true;
-            break;
-        case KEYS.UP:
-
-            UpDown = true;
-            break;
-        case KEYS.DOWN:
-
-            DownDown = true;
-            break;
+			sendRightUnit();
+			break;
         case KEYS.SPACE:
-            if (isGameActive == false) {
-                isGameActive = true;
-				startGame();
-            }
-			
+			startGame();
             break;
         case KEYS.P:
             if (isPaused == true) {
@@ -103,12 +42,30 @@ function doKeyDown(evt) {
             break;
     }
 }
-
-
-/////////////////////////////////////////////////////////////////////
-//                      Process Input
-////////////////////////////////////////////////////////////////////
-
-function processInput() {
-
+///////////////////////////////////////////////////////////////
+//Methods that do the work of input
+//////////////////////////////////////////////////////////////
+//this should really be sendunit(type), but we'll get there.
+function sendLeftUnit()
+{
+    if (leftTeamMoney > leftType.cost) {
+		var newLeftType = new UnitType(5,1,10,3,10,'left');
+		leftTeamUnits.push(new Unit(newLeftType, blueRobotSprite, 0, 400, ++unitCount));
+		leftTeamMoney -= leftType.cost;
+	}
 }
+function sendRightUnit()
+{
+    if (rightTeamMoney >= rightType.cost) {
+		var newRightType = new UnitType(5,-1,10,3,10,'right');
+		rightTeamUnits.push(new Unit(newRightType, pinkRobotSprite, Canvas.Width, 400, ++unitCount));
+		rightTeamMoney -= rightType.cost;
+    }
+}
+function start(){
+    if (isGameActive == false) {
+        isGameActive = true;
+		startGame();
+    }
+}
+
