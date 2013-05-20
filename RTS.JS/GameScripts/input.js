d51e10aaf1aa02e23fabfd39d2839fb4e3fc3b67
jmorrisosu <jmorrisosu@gmail.com>
2013-05-20 14:28:54 +0000

    significant refactoring.

--- RTS.JS/GameScripts/input.js
+++ RTS.JS/GameScripts/input.js
@@ -12,12 +12,12 @@ var KEYS = {
 //Key Listeners
 window.addEventListener('keydown', doKeyDown, true);
 
-$('#btn_leftA').on("click", sendLeftUnit);
-$('#btn_leftB').on("click", sendLeftArcher);
-$('#btn_rightA').on("click", sendRightUnit);
-$('#btn_rightB').on("click", sendRightArcher);
+//$('#btn_leftA').on("click", sendLeftUnit);
+//$('#btn_leftB').on("click", sendLeftArcher);
+//$('#btn_rightA').on("click", sendRightUnit);
+//$('#btn_rightB').on("click", sendRightArcher);
 
-$('#btn_start').on("click", start);
+//$('#btn_start').on("click", start);
 ////////////////////////////////////////////////////////////////////////////
 //                        Callbacks
 ///////////////////////////////////////////////////////////////////////////
@@ -25,27 +25,31 @@ $('#btn_start').on("click", start);
 function doKeyDown(evt) {
     switch (evt.keyCode) {     
         case KEYS.LEFT:
-			sendLeftUnit();
+            var newLeftType = new UnitType(5,1,10,3,35,10,'left');
+			sendUnit(leftPlayer,newLeftType,blueRobotSprite)
             break;    
         case KEYS.RIGHT:
-			sendRightUnit();
+            var newRightType = new UnitType(5,-1,10,3,35,10,'right');
+			sendUnit(rightPlayer,newRightType,pinkRobotSprite);
 			break;
         case KEYS.SPACE:
             start();
             break;
         case KEYS.UP:
-            sendLeftArcher();
+            var leftArcherType = new UnitType(5,1,10,3,100,10,'left');
+            sendUnit(leftPlayer,leftArcherType,archerSprite);
             break;
         case KEYS.DOWN:
-            sendRightArcher();
+            var rightArcherType = new UnitType(5,-1,10,3,100,10,'right');
+            sendUnit(rightPlayer, rightArcherType, archer2Sprite);
             break;
         case KEYS.P:
-            if (globals.isPaused == true) {
-                globals.isPaused = false;
+            if (game.isPaused == true) {
+                game.isPaused = false;
                 startGame();
             }
             else {
-                globals.isPaused = true;
+                game.isPaused = true;
             }
             break;
     }
@@ -54,44 +58,19 @@ function doKeyDown(evt) {
 //Methods that do the work of input
 //////////////////////////////////////////////////////////////
 //this should really be sendunit(type), but we'll get there.
-function sendLeftUnit()
-{
-    if (leftTeamMoney >= leftType.cost) {
-		var newLeftType = new UnitType(5,1,10,3,35,10,'left');
-		leftTeamUnits.push(new Unit(newLeftType, blueRobotSprite, 0, 400, ++unitCount));
-		leftTeamMoney -= leftType.cost;
-	}
-}
-function sendRightUnit()
-{
-    if (rightTeamMoney >= rightType.cost) {
-		var newRightType = new UnitType(5,-1,10,3,35,10,'right');
-		rightTeamUnits.push(new Unit(newRightType, pinkRobotSprite, Canvas.Width, 400, ++unitCount));
-		rightTeamMoney -= rightType.cost;
+function sendUnit(player,type,sprite){
+    if(player.money >= type.cost)
+    {
+        player.units.push(new Unit(type,sprite,player.baseX,player.baseY, ++unitCount))
     }
-}
-function sendLeftArcher()
-{
-    if(leftTeamMoney >= 20){
-        var leftArcherType = new UnitType(5,1,10,3,100,10,'left');
-        leftTeamUnits.push(new Unit(leftArcherType, archerSprite, 0, 400, ++unitCount));
-        leftTeamMoney -= 20;
-    }
-}
-function sendRightArcher()
-{
-    if(rightTeamMoney >= 20){
-        var rightArcherType = new UnitType(5,-1,10,3,100,10,'right');
-        rightTeamUnits.push(new Unit(rightArcherType, archer2Sprite, Canvas.Width, 400, ++unitCount));
-        rightTeamMoney -= 20;
-    }
+    player.money -= type.cost;
 }
 function start(){
-    if(!globals.isGameActive)
+    if(!game.isGameActive)
     {
         preinit();
         startGame();
-        globals.isGameActive = true;
+        game.isGameActive = true;
     }
 }
 
