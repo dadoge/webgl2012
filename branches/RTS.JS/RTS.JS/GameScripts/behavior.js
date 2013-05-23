7bfc5f0f89a9ebaec49240495d3f51f592e55d63
jmorrisosu <jmorrisosu@gmail.com>
2013-05-23 01:50:40 +0000

    Baby's first behavior.

---
+++ branches/RTS.JS/RTS.JS/GameScripts/behavior.js
@@ -0,0 +1,10 @@
+//var walkBehavior = {
+//   frames: [{x: 0, y:0},{x:1, y:0},{x:2, y:0},{x:3, y:0}],
+//   update: function(unit){
+//       unit.x+= unit.speed;
+//       if(this.state < 19)
+//           this.state++;
+//       else this.state = 0;
+//   },
+//   state: 0
+//
