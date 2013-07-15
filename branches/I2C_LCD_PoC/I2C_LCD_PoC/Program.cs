73ee07b811dc7dac369f658de92c995efe56d5d1
jmorrisosu <jmorrisosu@gmail.com>
2013-07-15 23:35:32 +0000

    Checking in sample code.

--- branches/I2C_LCD_PoC/I2C_LCD_PoC/Program.cs
+++ branches/I2C_LCD_PoC/I2C_LCD_PoC/Program.cs
@@ -11,25 +11,40 @@ namespace I2C_LCD_PoC
 {
     public class Program
     {
-        public static byte[] buffer = { 0x50 };
         public static void Main()
         {
-            // write your code here
-            
-            I2CDevice i2c = new I2CDevice(new I2CDevice.Configuration(0x50,100));
-            Microsoft.SPOT.Hardware.I2CDevice.I2CTransaction[] actions;
-            
 
-            actions = new I2CDevice.I2CTransaction[]
-            {
-                I2CDevice.CreateWriteTransaction(buffer)
-            };
+            OutputPort o = new OutputPort(Pins.ONBOARD_LED, true);
 
             while (true)
             {
-                i2c.Execute(actions, 1000);
-                Thread.Sleep(30);
+                Thread.Sleep(1000);
+                o.Write(true);
+                Thread.Sleep(1000);
+                o.Write(false);
             }
+            // write your code here
+            
+        //    I2CDevice i2c = new I2CDevice(new I2CDevice.Configuration(0x38,100));
+        //    Microsoft.SPOT.Hardware.I2CDevice.I2CTransaction[] actions;
+        //    var buffer = new byte[1];
+
+        //    actions = new I2CDevice.I2CTransaction[]
+        //    {
+        //        I2CDevice.CreateWriteTransaction(buffer)
+        //    };
+
+        //    while (true)
+        //    {
+        //        buffer[0] = 255;
+        //        i2c.Execute(actions, 1000);
+        //        Thread.Sleep(1000);
+
+
+        //        buffer[0] = 0;
+        //        i2c.Execute(actions, 1000);
+        //        Thread.Sleep(1000);
+        //    }
         }
 
     }
