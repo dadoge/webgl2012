476efd1f3100ee2da324906f61d72e88bee2d864
jmorrisosu <jmorrisosu@gmail.com>
2013-07-21 01:10:10 +0000

    rf reading updates.

--- LT_RFID/LT_RFID/Program.cs
+++ LT_RFID/LT_RFID/Program.cs
@@ -11,32 +11,39 @@ namespace LT_RFID
     public class Program
     {
         public static SerialPort SPort;
+         
         public static void Main()
         {
-            SerialPort SPort = new SerialPort(SerialPorts.COM2, 9600);
+            int count = 0;
+            SerialPort SPort = new SerialPort(SerialPorts.COM2, 9600, Parity.None,8,StopBits.One);
             SPort.ReadTimeout = 1000;
             SPort.WriteTimeout = 1000;
             byte[] buf = new byte[5];
             string CardId = "";
             SPort.Open();
-            SPort.Write(System.Text.Encoding.UTF8.GetBytes("!RW"), 0, 3);
-            SPort.Write(new byte[] {0x01, 0x20}, 0, 2);
+            byte[] readCommand = { 0x21, 0x52, 0x57, 0x01, 0x03 };
+            int numCodes = 0;
             while (true)
             {
                 //SPort.Write(new byte[] { 0xFF, 0xFF, 0X39, 0x44 }, 0, 4);
+                SPort.Write(readCommand, 0, 5);
+                SPort.Flush();
                 int readcnt = SPort.Read(buf, 0, SPort.BytesToRead);
+                SPort.Flush();
                 string s = "";
-                foreach(byte b in buf)
+                if (buf[0] == 0x01 && numCodes < 10)
                 {
-                    s = s + (b.ToString() + ",");
+                    foreach (byte b in buf)
+                    {
+                        s = s + b.ToString() + ",";
+                    }
+                    if (s[0] == '1')
+                    {
+                        count++;
+                        Debug.Print(count.ToString() + ":" + s + "\n");
+                        numCodes++;
+                    }
                 }
-                if (s[0] == '1')
-                {
-                    Debug.Print(s + "\n");
-                }
-                SPort.Write(System.Text.Encoding.UTF8.GetBytes("!RW"), 0, 3);
-                SPort.Write(new byte[] { 0x01, 0x21}, 0, 2);
-
             }
         }
     }
