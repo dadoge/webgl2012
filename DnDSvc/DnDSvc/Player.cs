b6728a4be79a5c6d218f97e19e671eb15b71e7f4
jmorrisosu <jmorrisosu@gmail.com>
2014-03-11 23:47:50 +0000

    DnD Service strawman.

---
+++ DnDSvc/DnDSvc/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace DnDSvc
+{
+    // TODO: Edit the SampleItem class
+    public class Player
+    {
+        public int Id { get; set; }
+        public string StringValue { get; set; }
+
+        public Player(int id)
+        {
+            Id = id;
+            StringValue = "Player " + Id + "is Gay";
+        }
+        public Player()
+        {
+            Id = 0;
+            StringValue = "Player " + Id + "is Gay";
+        }
+    }
+}
