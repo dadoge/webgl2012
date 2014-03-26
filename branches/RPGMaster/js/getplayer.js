29a8bec7088f8d921bc6af96bc8f0bd7da5bcd86
antoncmorgan <antoncmorgan@gmail.com>
2014-03-26 00:26:27 +0000

    dev on hiding character div

--- branches/RPGMaster/js/getplayer.js
+++ branches/RPGMaster/js/getplayer.js
@@ -1,5 +1,5 @@
 $("#footer").click(function () {
-	$.getJSON("http://antonmorgan.com/dndsvc/getplayer/18", function (data) {
+	$.getJSON("http://www.antonmorgan.com/dndsvc/getplayer/18", function (data) {
 		var change = "Name: " + data.Name + "<br/>Health: " + data.Health + "<br/>INT: " + data.Intelligence;
 		document.getElementById('character-info').innerHTML=change;
 		//document.getElementById('character-info').innerHTML="Test";
--- branches/RPGMaster/js/hidemenu.js
+++ branches/RPGMaster/js/hidemenu.js
@@ -8,6 +8,15 @@ $( "#menu-tab").click(function() {
 		$( "#menu-container" ).animate( {"left":"+=160px"}, "slow" );
 	}
 });
-	function HideCharacterMenu(){
-	
-	}
+/* $( "#character-hide").click(function() {
+	// Set the effect type
+    var effect = 'slide';
+ 
+    // Set the options for the effect type chosen
+    var options = { direction: 'right' };
+ 
+    // Set the duration (default: 400 milliseconds)
+    var duration = 700;
+ 
+    $("#character-all").toggle(effect, options, duration);
+}); */
