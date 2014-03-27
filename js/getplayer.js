84a75785733b0d7449a03e40090c5e94f3d14f6f
antoncmorgan <antoncmorgan@gmail.com>
2014-03-27 03:25:39 +0000

    Initial Commit

--- js/getplayer.js
+++ js/getplayer.js
@@ -1,7 +1,7 @@
 $("#footer").click(function () {
 	$.getJSON("http://www.antonmorgan.com/dndsvc/getplayer/18", function (data) {
 		var change = "Name: " + data.Name + "<br/>Health: " + data.Health + "<br/>INT: " + data.Intelligence;
-		document.getElementById('character-info').innerHTML=change;
+		document.getElementById('character-tab').innerHTML=change;
 		//document.getElementById('character-info').innerHTML="Test";
 		//$('#character-info').append(data.Name);
 	});
