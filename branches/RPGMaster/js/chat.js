ca6c7e32a9c5e24e03782ecb79040539ba43d2b7
antoncmorgan@gmail.com <antoncmorgan@gmail.com@77a4fc73-e944-9af1-8b44-5fee765067de>
2014-03-28 20:21:23 +0000

    Removed function() around top level of stompClient.connect. Added SendData function to perform actual send via stompClient.send. In .keypress, added if to check if username input field is blank. Prevents user from sending a message without typing in an username.

--- branches/RPGMaster/js/chat.js
+++ branches/RPGMaster/js/chat.js
@@ -1,44 +1,34 @@
-/* function AppendChat() {
-	$( "#chat" ).append( "<p>Test</p>" );
-	$("#chat").scrollTop($("#chat")[0].scrollHeight);
-}; */
-
 	$( document ).ready(function() {
 		var stompClient;
 		var i = 0;
 		var Channel = "/topic/TonysChannel";
-		(function() {
-			var socket = new SockJS('http://www.simplert.com:8100/ws');
-			stompClient = Stomp.over(socket);
+		var socket = new SockJS('http://www.simplert.com:8100/ws');
+		stompClient = Stomp.over(socket);
+		
+		stompClient.connect({}, function(frame) {
+			stompClient.subscribe(Channel, function(message) {
+				$('<div/>').text(" [" + moment().format('HH:mm:ss') +"] " + message.body).appendTo('#chat');
+				$("#chat").scrollTop($("#chat")[0].scrollHeight);
+			});
+		  }, function(error) {
+			console.log("Connection error " + error);
+		  });
 			
-			stompClient.connect({}, function(frame) {
-				stompClient.subscribe(Channel, function(message) {
-					//var today = new Date();
-					$('<div/>').text(" [" + moment().format('HH:mm:ss') +"] " + message.body).appendTo('#chat');
-					$("#chat").scrollTop($("#chat")[0].scrollHeight);
-					//$( "#chat" ).append( message.body);
-					//+ $('#chatname-input').val() + ": "
-				});
-			  }, function(error) {
-				console.log("Connection error " + error);
-			  });
-			
-			/*$('#character-hide').on('click', function(){
-				 stompClient.send(Channel, {}, $('#chat-input').val());
-				 //
-			 });
-			*/
-				$("#chat-input").keypress(function(event){
-				  if(event && event.keyCode == 13)
-				   {
-						var senddata= $('#chatname-input').val() + ": " + $('#chat-input').val();
-						stompClient.send(Channel, {}, senddata);
-						$('#chat-input').val("");
-				   }
-				}); 
-		//	$('#chat-input').val()
-				
-				//
-			
-		})();
+			$("#chat-input").keypress(function(event){
+			  if(event && event.keyCode == 13)
+			   {
+					if ($('#chatname-input').val()=="") {
+						alert("Please enter in username.")
+					}
+					else {
+					var senddata= $('#chatname-input').val() + ": " + $('#chat-input').val();
+					SendData(senddata);
+					$('#chat-input').val("");
+					}
+			   }
+			}); 
+			function SendData(data){
+				stompClient.send(Channel, {}, data);
+				return 0;
+			}
 	});
