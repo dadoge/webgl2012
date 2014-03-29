81a519ebb1c088594adf65ab60328737991791bd
jmorrisosu@gmail.com <jmorrisosu@gmail.com@77a4fc73-e944-9af1-8b44-5fee765067de>
2014-03-29 14:18:46 +0000

    Quick and dirty data layer pattern.  Uses parameterized stored procedures to populate entities objects using a looping data reader.  Feel free to follow this pattern for now.
    We will want to implement more complex architecture as we move along.

--- branches/RPGSvc/RPGSvc/Data/StoredSkill.cs
+++ branches/RPGSvc/RPGSvc/Data/StoredSkill.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using RPGSvc.Entities;
+using System.Data.SqlClient;
 
 namespace RPGSvc.Data
 {
@@ -15,15 +16,44 @@ namespace RPGSvc.Data
             //search C# datareader
             //called storage procedure c#
             //values being passed into new Skill would be from database
-            var skill1 = new Skill("1", "Stealth", "Ability to sneak around", 18);
-            var skill2 = new Skill("2", "Swim", "How well you can swim", 8);
-            var skill3 = new Skill("3", "Run", "How well you can run", 12);
+            //var skill1 = new Skill("1", "Stealth", "Ability to sneak around", 18);
+            //var skill2 = new Skill("2", "Swim", "How well you can swim", 8);
+            //var skill3 = new Skill("3", "Run", "How well you can run", 12);
+
+            //var skillList = new List<Skill>();
+            //skillList.Add(skill1);
+            //skillList.Add(skill2);
+            //skillList.Add(skill3);
+
+            //return skillList;
+            SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["RPGMasterDb"].ConnectionString);
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "GetSkillsByPlayerID";
+            command.CommandType = CommandType.StoredProcedure;
+
+ 
+            SqlParameter playerID = new SqlParameter("@PlayerID", SqlDbType.Int);
+            playerID.Value = 1;
+            command.Parameters.Add(playerID);
+
+            connection.Open();
+            SqlDataReader dr;
+            dr = command.ExecuteReader();
 
             var skillList = new List<Skill>();
-            skillList.Add(skill1);
-            skillList.Add(skill2);
-            skillList.Add(skill3);
 
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    var skill = new Skill();
+                    skill.Id = dr.GetInt32(0).ToString();
+                    skill.Name = dr.GetString(1);
+                    skill.Value = dr.GetDouble(2);
+                }
+            }
+            connection.Close();
             return skillList;
         }
 
