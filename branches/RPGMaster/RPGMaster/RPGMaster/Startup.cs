4b74725b75c5ad19541d29c3d1f33581c4178c38
antoncmorgan <antoncmorgan@gmail.com>
2014-04-02 01:24:20 +0000

    Created template from VS for webforms. Will be merging work done so far into this to start creating full web app.

---
+++ branches/RPGMaster/RPGMaster/RPGMaster/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RPGMaster.Startup))]
+namespace RPGMaster
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
