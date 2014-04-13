ea3a4d36383fe866b78da6208ecc59f56ee0a8cf
antoncmorgan@gmail.com <antoncmorgan@gmail.com@77a4fc73-e944-9af1-8b44-5fee765067de>
2014-04-13 16:04:38 +0000

    Added Data, Game, Images and Lib folder. Game is where game related pages will be added, such as the Game Lobby, with the interactive screen. Data is where classes will exists for access to the database.

---
+++ branches/RPGMaster/RPGMaster/RPGMaster/Data/StoredAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace RPGMaster.Data
+{
+    public class StoredAccount
+    {
+        public void CreateNewAccount(string userID, string email)
+        {
+
+            SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["RPGMasterDb"].ConnectionString);
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "CreateNewAccount";
+            command.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter UserID = new SqlParameter("@UserID", SqlDbType.NVarChar);
+            SqlParameter Email = new SqlParameter("@Email", SqlDbType.NVarChar);
+
+            UserID.Value = userID;
+            Email.Value = email;
+
+            command.Parameters.Add(UserID);
+            command.Parameters.Add(Email);
+
+            connection.Open();
+            //SqlDataReader dr;
+            //dr = command.ExecuteReader();
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
