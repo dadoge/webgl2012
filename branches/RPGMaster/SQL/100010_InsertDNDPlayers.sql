984e42bbdf0950d88c4869684d4198a870bc5c39
antoncmorgan <antoncmorgan@gmail.com>
2014-04-16 00:43:52 +0000

    Created Schema to include ASPNet roles and numbering.

---
+++ branches/RPGMaster/SQL/100010_InsertDNDPlayers.sql
@@ -0,0 +1,13 @@
+USE [rpgmaster]
+
+INSERT INTO Player (Name)
+VALUES ('Iargalon');
+
+INSERT INTO Player (Name)
+VALUES ('Xana');
+
+INSERT INTO Player (Name)
+VALUES ('Jessie');
+
+INSERT INTO Player (Name)
+VALUES ('Jacob');
