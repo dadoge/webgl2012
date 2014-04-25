c8c27aea3aa13e26388da424c3e5a8b9c5c95a2a
antoncmorgan <antoncmorgan@gmail.com>
2014-04-25 01:21:52 +0000

    Moved Data_Scripts to appropriate folder to separate schema and data inserts for testings. Added and Altered Player table and associative tables for character creation

--- SQL/Data_Scripts/100020-InsertDNDStats.sql
+++ SQL/Data_Scripts/100020-InsertDNDStats.sql
@@ -1,7 +1,7 @@
 USE [rpgmaster]
 
 INSERT INTO Stat (Name, Description)
-VALUES ('Strength', 'An ability score representing a character''s muscle power, edurance, and stamina.');
+VALUES ('Strength', 'An ability score representing a character''s muscle power, endurance, and stamina.');
 
 INSERT INTO Stat (Name, Description)
 VALUES ('Dexterity', 'An ability score representing a combination of a character''s agility, reflexes, hand-eye coordination, and the like.');
@@ -13,7 +13,7 @@ INSERT INTO Stat (Name, Description)
 VALUES ('Intelligence', 'An ability score representing a character''s memory, reasoning, and learning ability.');
 
 INSERT INTO Stat (Name, Description)
-VALUES ('Wisdom', 'An ability score representing a composite of a chracter''s intuition, judgement, common sense, and will power.');
+VALUES ('Wisdom', 'An ability score representing a composite of a character''s intuition, judgement, common sense, and will power.');
 
 INSERT INTO Stat (Name, Description)
 VALUES ('Charisma', 'An ability score representing a character''s persuasiveness, personal magnetism, and ability to read.');
