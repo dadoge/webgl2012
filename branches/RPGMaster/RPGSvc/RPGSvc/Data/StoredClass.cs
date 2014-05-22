dee889d1f6a21d92841c5a9a874b3a0f8aa62bb6
antoncmorgan <antoncmorgan@gmail.com>
2014-05-22 23:12:34 +0000

    Added machinery for getting information about a new character. Including, classes for Alignment, Class, Gender, Races, etc. Create a New Character Repository class.

---
+++ branches/RPGMaster/RPGSvc/RPGSvc/Data/StoredClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using RPGSvc.Entities;
+using System.Data.SqlClient;
+
+namespace RPGSvc.Data
+{
+    public class StoredClass
+    {
+        public List<Class> GetClasses()
+        {
+
+            SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["RPGMasterDb"].ConnectionString);
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "GetClasses";
+            command.CommandType = CommandType.StoredProcedure;
+
+            connection.Open();
+            SqlDataReader dr;
+            dr = command.ExecuteReader();
+
+            var classList = new List<Class>();
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    var class_ = new Class();
+                    class_.Id = dr.GetInt32(0);
+                    class_.Name = dr.GetString(1);
+                    class_.Description = dr.GetString(2);
+
+                    classList.Add(class_);
+                }
+            }
+            connection.Close();
+            return classList;
+        }
+    }
+}
---
+++ branches/RPGMaster/RPGSvc/RPGSvc/Repositories/NewCharacterRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RPGSvc.Entities;
+using RPGSvc.Data;
+
+namespace RPGSvc.Repositories
+{
+    public class NewCharacterRepository
+    {
+        public NewCharacterRepository()
+        {
+            //initialize various data objects here
+        }
+
+        public NewCharacter GetNewCharacter()
+        {
+            //Get player,player skills and player stats
+            var newCharacter = new NewCharacter();
+            newCharacter.Races = new StoredRaces().GetRaces();
+            newCharacter.Stats = new StoredStat().GetStats();
+            newCharacter.Genders = new StoredGender().GetGenders();
+            newCharacter.Alignments = new StoredAlignment().GetAlignments();
+            newCharacter.Classes = new StoredClass().GetClasses();
+
+            return newCharacter;
+        }
+    }
+}
