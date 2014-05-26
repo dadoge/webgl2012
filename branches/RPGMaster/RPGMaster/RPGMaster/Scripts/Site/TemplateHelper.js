10b3e4b961598ea130404607d1d618c640f6d3d2
antoncmorgan <antoncmorgan@gmail.com>
2014-05-26 02:38:42 +0000

    Added knockout for data binding to DOM elements in order to handle dynamic data viewing and manipulation. modified Create_Character.js with classes for knockout. Modified site.js for dynamic loading of createcharacter screens using knockout. Have first prototype patter of using knockout created.

--- branches/RPGMaster/RPGMaster/RPGMaster/Scripts/Site/TemplateHelper.js
+++ branches/RPGMaster/RPGMaster/RPGMaster/Scripts/Site/TemplateHelper.js
@@ -25,7 +25,7 @@
     this.startCreation = function () {
         var htmlTemplate = " \
             <div class=\"interactive-inner\"> \
-                <div class=\"text-shadow\">Create New Character? </div> \
+                <div class=\"text-shadow\"><h5>Create New Character?</h5> </div> \
                 <div class=\"btn-group\"> \
                     <button id=\"startCreation-Yes\" type=\"button\" class=\"btn btn-default btn-width btn-shadow\">Yes</button> \
                 </div> \
@@ -34,25 +34,6 @@
                 </div> \
             </div> ";
             
-        return htmlTemplate;
-    }
-
-
-    this.startCreation_btns = function () {
-        var htmlTemplate = " <div id=\"createCharacter-h\" class=\"text-shadow\"></div> \
-            <div class=\"createCharacter-btns\"> \
-                <div id=\"createCharacter-Back class=\"btn-group\"> \
-                </div> \
-                <div class=\"btn-group\"> \
-                    <button id=\"CreateCharacter-Next\" type=\"button\" class=\"btn btn-default btn-width btn-shadow\">Next</button> \
-                </div> \
-                <div class=\"btn-group\"> \
-                    <button id=\"CreateCharacter-Cancel\" type=\"button\" class=\"btn btn-default btn-width btn-shadow\">Cancel</button> \
-                </div> \
-            </div> \
-            <div id=\"createCharacter-inner\"> \
-            </div> ";
-
         return htmlTemplate;
     }
 
@@ -83,11 +64,14 @@
         }
 
         var GetContent = function (Races) {
-            var htmlTemplate = "<% _.each(Races, function(Races) { %> \
-                <div id=\"<%= Races.Id %>\" class=\"RaceSelector\" title=\"<%= Races.Description %>\"> \
-                    <%= Races.Name %> \
-                </div> \
-            <% }); %>";
+            var htmlTemplate = "<div id=\"Race_div\">\
+                <% _.each(Races, function(Races) { %> \
+                    <div id=\"<%= Races.Id %>\" class=\"RaceSelector\" title=\"<%= Races.Description %>\"> \
+                        <img id=\"RaceImg\" src=\"/Images/<%= Races.ImgSrc %>\" />\
+                        <%= Races.Name %> \
+                    </div> \
+                <% }); %> \
+            </div> ";
 
             return _.template(htmlTemplate, Races);
         }
@@ -120,20 +104,38 @@
     }
 
     this.createIdentity = function (Alignments, Genders) {
-        var temp = "<div class=\"text-shadow\">Select Stats </div> \
-            <div class=\"btn-group\"> \
-                <button id=\"CreateCharacter-Next\" type=\"button\" class=\"btn btn-default btn-width btn-shadow\">Next</button> \
-            </div> \
-            <div class=\"btn-group\"> \
-                <button id=\"CreateCharacter-Cancel\" type=\"button\" class=\"btn btn-default btn-width btn-shadow\">Cancel</button> \
-            </div> \
-        <% _.each(Stats, function(Stats) { %> \
-            <div id=\"<%= Stats.Id %>\" class=\"StatsSelector\" title=\"<%= Stats.Description %>\"> \
-                <%= Stats.Name %> \
-            </div> \
-        <% }); %>";
+        var returnObj = new Object();
+        var GetHeader = function () {
+            return "<h3>Define Character</h3>";
+        }
 
-        return _.template(temp, Stats);
+        var GetContent = function (Alignments, Genders) {
+            //var htmlTemplate = "Alignment <select data-bind=\"value: Alignment\" id=\"Alignment-options\" class=\"form-control input-sm\"> \
+            //        <% _.each(Alignments, function(Alignments) { %> \
+            //                <option value=\"<%= Alignments.Id %>\" id=\"<%= Alignments.Id %>\" title=\"<%= Alignments.Description %>\"><%= Alignments.Name %> </option> \
+            //            </div> \
+            //        <% }); %> \
+            //</select>";
+            var htmlTemplate = "<div id=\"createIdentity\">Alignment <select data-bind=\"options: availableAlignments, value: Alignment, optionsText: 'Name'\" id=\"Alignment-options\" class=\"form-control input-sm\"> \
+            </select>";
+            var htmlTemplate2 = "Gender <select data-bind=\"options: availableGenders, value: Gender, optionsText: 'Name'\" id=\"Gender-options\" class=\"form-control input-sm\"> \
+            </select> \
+            </div>";
+            
+            return (_.template(htmlTemplate, Alignments) + _.template(htmlTemplate2, Genders));
+        }
+        
+        var GetNameField = function () {
+            return "<div class=\"input-group createCharacter-input-group\"> \
+						Character Name <input id=\"createCharacter-Name-input\" type=\"text\" class=\"form-control\" placeholder=\"Name\"> \
+					</div> ";
+        }
+        
+        returnObj.GetContent = GetContent(Alignments, Genders);
+        returnObj.GetHeader = GetHeader();
+        returnObj.GetNameFiled = GetNameField();
+
+        return returnObj;
     }
 
 
