f1f29f62334006c341d66cbda84057331fdf416e
antoncmorgan <antoncmorgan@gmail.com>
2014-05-26 02:47:42 +0000

    Added ImgSrc Columns to tables

---
+++ branches/RPGMaster/SQL/100200-AddImageField.sql
@@ -0,0 +1,18 @@
+USE [rpgmaster]
+
+GO
+
+/****** Object:  Tables Script Date:5/22/2014 7:24:30 PM ******/
+ALTER TABLE [dbo].Alignment ADD [ImgSrc] [nvarchar](1024) NULL;
+GO
+ALTER TABLE [dbo].Class ADD [ImgSrc] [nvarchar](1024) NULL;
+GO
+ALTER TABLE [dbo].Gender ADD [ImgSrc] [nvarchar](1024) NULL;
+GO
+ALTER TABLE [dbo].Race ADD [ImgSrc] [nvarchar](1024) NULL;
+GO
+ALTER TABLE [dbo].Spell ADD [ImgSrc] [nvarchar](1024) NULL;
+GO
+ALTER TABLE [dbo].Skill ADD [ImgSrc] [nvarchar](1024) NULL;
+GO
+
