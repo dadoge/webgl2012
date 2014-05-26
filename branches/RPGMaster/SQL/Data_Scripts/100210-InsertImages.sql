7a4a668d0647e65dd73d7cc839160c6937c765a7
antoncmorgan@gmail.com <antoncmorgan@gmail.com@77a4fc73-e944-9af1-8b44-5fee765067de>
2014-05-26 02:49:02 +0000

    Created script to add image source files of races

---
+++ branches/RPGMaster/SQL/Data_Scripts/100210-InsertImages.sql
@@ -0,0 +1,8 @@
+USE [rpgmaster]
+
+UPDATE Race SET ImgSrc = 'Race_Dwarf.png' WHERE RaceID=1;
+UPDATE Race SET ImgSrc = 'Race_Elf.png' WHERE RaceID=2;
+UPDATE Race SET ImgSrc = 'Race_Gnome.png' WHERE RaceID=3;
+UPDATE Race SET ImgSrc = 'Race_Half-Elf.png' WHERE RaceID=4;
+UPDATE Race SET ImgSrc = 'Race_Halfling.png' WHERE RaceID=5;
+UPDATE Race SET ImgSrc = 'Race_Human.png' WHERE RaceID=6;
