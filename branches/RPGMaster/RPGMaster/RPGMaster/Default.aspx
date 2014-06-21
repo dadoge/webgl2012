e2b32ed253a3aabd1d377a6bf8cb5df096a140e2
antoncmorgan@gmail.com <antoncmorgan@gmail.com@77a4fc73-e944-9af1-8b44-5fee765067de>
2014-06-21 20:01:53 +0000

    Modifications to User-Home and background js for character creation. This update is a near complete character creation screens. Changed templates and gets to be able to get logged in user information, such as chat name and active character. Implementation of Knockout-js is used for databinding of character creation screen and underscore for further assistance on templating.

--- branches/RPGMaster/RPGMaster/RPGMaster/Default.aspx
+++ branches/RPGMaster/RPGMaster/RPGMaster/Default.aspx
@@ -1,18 +1,14 @@
-﻿<%@ Page Title="Home" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RPGMaster._Default" %>
+﻿<%@ Page Title="Welcome" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RPGMaster._Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     
-<div class="container container-back container-custom">
-    <div style="font-family: Sketch-Gothic;font-size: 40px">Iargalon Log in Test DEX STR Username Password General words and page</div>
-    <%--<div style="font-family: Wizard-Of-The-Moon;font-size: 40px">Log in Test DEX STR Username Password General words and page</div>--%>
-    <%--<div style="font-family: Celtic-Custom;font-size: 20px">Log in Test DEX STR Username Password General words and page</div>--%>
-    <%--<div style="font-family: Dorovar-Carolus;font-size: 30px">Log in Test DEX STR Username Password General words and page</div>--%>
-    <%--<div style="font-family: Baldur;font-size: 20px">Log in Test DEX STR Username Password General words and page</div>--%>
-    <div style="font-family: Ringbearer;font-size: 30px">Iargalon Log in Test dex str wis DEX STR Username Password General words and page</div>
-    <%--<div style="font-family: Didact-Gothic;font-size: 30px">Log in Test dex str wis DEX STR Username Password General words and page</div>--%>
-    <div style="font-family: Leo-Arrow;font-size: 30px">Iargalon Log in Test dex str wis DEX STR Username Password General words and page</div>
-    
+<div class="container account-wrapper">
+    <div class="container container-back account-main-container">
+        <div class="account-inner-container text-center">
+        <h3><%: Title %> to RPG Master</h3>
+            <p>Please <a runat="server" href="~/Account/LogIn">Log in</a> or <a runat="server" href="~/Account/Register">Register</a> a new account!</p>
+        </div>
+    </div>
 </div>
-
 
 </asp:Content>
--- branches/RPGMaster/RPGMaster/RPGMaster/Scripts/Site/TemplateHelper.js
+++ branches/RPGMaster/RPGMaster/RPGMaster/Scripts/Site/TemplateHelper.js
@@ -1,12 +1,35 @@
 ﻿function TemplateHelper() {
 
+    this.getCharFooterHtml = function(Name,Level){
+        var html1 = "<div class=\"character-Name\"> <%= Name %> </div>";
+        var html2 = "<div class=\"character-Level\"> Level: <%= Level %> </div>";
+
+        return (_.template(html1, Name) + _.template(html2, Level));
+    }
+
+    this.getGeneralHtml = function (Class, Race, Alignment, Age, History) {
+        var html1 = "<div class=\"character-Class\"> <div class=\"character-Description\">Class</div> : <%= Class %> </div>";
+        var html2 = "<div class=\"character-Race\"> <div class=\"character-Description\">Race</div> : <%= Race %> </div>";
+        var html3 = "<div class=\"character-Alignment\"> <div class=\"character-Description\">Alignment</div> : <%= Alignment %> </div>";
+        var html4 = "<div class=\"character-Age\"> <div class=\"character-Description\">Age</div> : <%= Age %> </div>";
+        var html5 = "<div class=\"character-History\"> <div class=\"character-Description\">History</div> : <%= History %> </div>";
+
+        return (_.template(html1, Class) + _.template(html2, Race) + _.template(html3, Alignment) + _.template(html4, Age) + _.template(html5, History));
+    }
+
     this.getStatsHtml = function(Stats){
-        var list = " \
+        var list = " <div class=\"character-stats-wrapper\"> \
             <% _.each(Stats, function(Stats) { %> \
             <li style=\"list-style-type: none\"> \
-                <%= Stats.Name.substring(0,3) %> : <%= Stats.Value %> \
+                <div class=\"character-stats-name\"> \
+                    <%= Stats.Name.substring(0,3) %> \
+                </div> \
+                <div class=\"character-stats-value\"> \
+                    <%= Stats.Value %> \
+                </div> \
             </li> \
-        <% }); %>";
+        <% }); %> \
+        </div>";
 
         return _.template(list, Stats);
     }
@@ -44,6 +67,9 @@
                     <button id=\"createCharacter-Back\" type=\"button\" class=\"btn btn-default btn-width btn-shadow\">Back</button> \
                 </div> \
                 <div class=\"btn-group\"> \
+                    <button id=\"createCharacter-Complete\" type=\"button\" class=\"btn btn-default btn-width btn-shadow\">Done</button> \
+                </div> \
+                <div class=\"btn-group\"> \
                     <button id=\"createCharacter-Next\" type=\"button\" class=\"btn btn-default btn-width btn-shadow\">Next</button> \
                 </div> \
                 <div class=\"btn-group\"> \
@@ -56,7 +82,6 @@
         return htmlTemplate;
     }
 
-
     this.selectRace = function (Races) {
         var returnObj = new Object();
         var GetHeader = function () {
@@ -64,10 +89,10 @@
         }
 
         var GetContent = function (Races) {
-            var htmlTemplate = "<div id=\"Race_div\">\
+            var htmlTemplate = "<div id=\"createRace\">\
                 <% _.each(Races, function(Races) { %> \
-                    <div id=\"<%= Races.Id %>\" class=\"RaceSelector\" title=\"<%= Races.Description %>\"> \
-                        <img id=\"RaceImg\" src=\"/Images/<%= Races.ImgSrc %>\" />\
+                    <div class=\"RaceSelector\" title=\"<%= Races.Description %>\"> \
+                        <img data-bind=\"click: SelectRace\" id=\"<%= Races.Id %>\" class=\"RaceImg\" src=\"../Images/<%= Races.ImgSrc %>\" />\
                         <%= Races.Name %> \
                     </div> \
                 <% }); %> \
@@ -81,6 +106,78 @@
         return returnObj;
     }
 
+    this.selectClass = function (Classes) {
+        var returnObj = new Object();
+        var GetHeader = function () {
+            return "<h3>Select Class</h3>";
+        }
+
+        var GetContent = function (Classes) {
+            var htmlTemplate = "<div id=\"createClass\">\
+                <% _.each(Classes, function(Classes) { %> \
+                    <div class=\"RaceSelector\" title=\"<%= Classes.Description %>\"> \
+                        <img data-bind=\"click: SelectClass\" id=\"<%= Classes.Id %>\" class=\"RaceImg\" src=\"../Images/<%= Classes.ImgSrc %>\" />\
+                        <%= Classes.Name %> \
+                    </div> \
+                <% }); %> \
+            </div> ";
+
+            return _.template(htmlTemplate, Classes);
+        }
+        returnObj.GetContent = GetContent(Classes);
+        returnObj.GetHeader = GetHeader();
+
+        return returnObj;
+    }
+
+    this.selectSkills = function (Skills) {
+        var returnObj = new Object();
+        var GetHeader = function () {
+            return "<h3>Select Skills</h3>";
+        }
+
+        var GetContent = function (Skills) {
+            var htmlTemplate = "<div id=\"selectSkills\">\
+                <ul data-bind=\"foreach: Skills\"> \
+                    <li> \
+                        <div class=\"checkbox-div\" data-bind=\"click: $parent.ChangeSelection,attr: {id: ('SkillCheckbox-' + Id)}\"> </div>\
+                        <input class=\"input-skillsValue\" data-bind=\"value: Value,visible: SelectedSkill,attr: {id: ('SkillValue-' + Id)}\" /> \
+                        <div class=\"selectSkills-Name\" data-bind=\"text: Name,attr: {id: ('SkillName-' + Id)}\"> </div> \
+                    </li> \
+                </ul> \
+            </div> ";
+
+            return _.template(htmlTemplate, Skills);
+        }
+        returnObj.GetContent = GetContent(Skills);
+        returnObj.GetHeader = GetHeader();
+
+        return returnObj;
+    }
+    this.selectFeats = function (Feats) {
+        var returnObj = new Object();
+        var GetHeader = function () {
+            return "<h3>Select Feats</h3>";
+        }
+
+        var GetContent = function (Feats) {
+            var htmlTemplate = "<div id=\"selectFeats\">\
+                <ul data-bind=\"foreach: Feats\"> \
+                    <li> \
+                        <input id=\"selectedFeats-input\" type=\"checkbox\" data-bind=\"checked: Selected\" /> \
+                        <div id=\"selectFeats-Name\" data-bind=\"text: Name\"> </div> \
+                        <div data-bind=\"attr: {title: Description}\" class=\"info-Description\"><span class=\"glyphicon glyphicon-info-sign\"></span></div> \
+                    </li> \
+                </ul> \
+            </div> ";
+
+            return _.template(htmlTemplate, Feats);
+        }
+        returnObj.GetContent = GetContent(Feats);
+        returnObj.GetHeader = GetHeader();
+
+        return returnObj;
+    }
 
     this.selectStats = function (Stats) {
         var returnObj = new Object();
@@ -89,11 +186,13 @@
         }
 
         var GetContent = function (Stats) {
-            var htmlTemplate = "<% _.each(Stats, function(Stats) { %> \
-                <div id=\"<%= Stats.Id %>\" class=\"StatsSelector\" title=\"<%= Stats.Description %>\"> \
-                    <%= Stats.Name %> \
+            var htmlTemplate = "<div id=\"createStats\"> \
+                <div data-bind=\"foreach: Stats\"> \
+                    <div class=\"div-createStats\"> \
+                        <h5 class=\"h-createStats\" data-bind=\"text: Name, attr: {title: Description}\"></h5><input class=\"input-createStats\" data-bind=\"value: Value, attr: {title: Description}\" /> \
+                    </div> \
                 </div> \
-            <% }); %>";
+            </div> ";
 
             return _.template(htmlTemplate, Stats);
         }
@@ -110,17 +209,17 @@
         }
 
         var GetContent = function (Alignments, Genders) {
-            //var htmlTemplate = "Alignment <select data-bind=\"value: Alignment\" id=\"Alignment-options\" class=\"form-control input-sm\"> \
-            //        <% _.each(Alignments, function(Alignments) { %> \
-            //                <option value=\"<%= Alignments.Id %>\" id=\"<%= Alignments.Id %>\" title=\"<%= Alignments.Description %>\"><%= Alignments.Name %> </option> \
-            //            </div> \
-            //        <% }); %> \
-            //</select>";
-            var htmlTemplate = "<div id=\"createIdentity\">Alignment <select data-bind=\"options: availableAlignments, value: Alignment, optionsText: 'Name'\" id=\"Alignment-options\" class=\"form-control input-sm\"> \
-            </select>";
-            var htmlTemplate2 = "Gender <select data-bind=\"options: availableGenders, value: Gender, optionsText: 'Name'\" id=\"Gender-options\" class=\"form-control input-sm\"> \
+            var htmlTemplate = "<div id=\"createIdentity\"> \
+            <div id=\"createIdentity-title\" >Name </div><input class=\"input-createName\" data-bind=\"value: Name\" /> <br /> \
+            <div id=\"createIdentity-title\" >Alignment </div><select data-bind=\"options: availableAlignments, value: Alignment, optionsText: 'Name'\" id=\"Alignment-options\" class=\"form-control input-sm  info-Description\"> \
             </select> \
+            <div data-bind=\"attr: {title: Alignment().Description}\" class=\"info-Description\"><span class=\"glyphicon glyphicon-info-sign\"></span></div>";
+            var htmlTemplate2 = "<br /><div id=\"createIdentity-title\" >Age </div><input class=\"input-createAge\" data-bind=\"value: Age\" /> Height  <input class=\"input-createAge\" data-bind=\"value: Height\" /> Weight  <input class=\"input-createAge\" data-bind=\"value: Weight\" />\
+            <br /><div id=\"createIdentity-title\" >Gender </div><select data-bind=\"options: availableGenders, value: Gender, optionsText: 'Name'\" id=\"Gender-options\" class=\"form-control input-sm  info-Description\"> \
+            </select> \
+            <br /><div id=\"createIdentity-title\" >History </div><br /><textarea class=\"input-createHistory\" data-bind=\"value: History\" /> \
             </div>";
+
             
             return (_.template(htmlTemplate, Alignments) + _.template(htmlTemplate2, Genders));
         }
