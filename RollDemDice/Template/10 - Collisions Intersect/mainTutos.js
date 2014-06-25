73b8b4e8e9a0b0fc2215b28de35ce8749cd39cf3
antoncmorgan <antoncmorgan@gmail.com>
2014-06-25 00:45:05 +0000

    First demonstration of using Babylon.js based off of demos at https://github.com/babylonjs/babylon.js/wiki

---
+++ RollDemDice/Template/10 - Collisions Intersect/mainTutos.js
@@ -0,0 +1,33 @@
+﻿window.onload = function () {
+
+    var canvas = document.getElementById("renderCanvas");
+
+    // Check support
+    if (!BABYLON.Engine.isSupported()) {
+        window.alert('Browser not supported');
+    } else {
+        // Babylon
+        var engine = new BABYLON.Engine(canvas, true);
+
+        //Load the collision tuto
+        // --------------------
+        scene = createSceneTutoCollisions(engine);
+        // --------------------
+
+        //Attach the camera to the scene
+        scene.activeCamera.attachControl(canvas);
+
+
+        // Once the scene is loaded, just register a render loop to render it
+        engine.runRenderLoop(function () {
+            scene.render();
+        });
+
+        // Resize
+        window.addEventListener("resize", function () {
+            engine.resize();
+        });
+    }
+
+};
+
