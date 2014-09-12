275423056b50345d2d518bb7bca50d6343a73026
jmorrisosu <jmorrisosu@gmail.com>
2014-09-12 00:35:14 +0000

    Initial implementation of SignalR chat in place of old Stomp client with simplert.
    Please follow patterns for creating methods on the hub and for wiring events after the creation of rpgprox and inside of the connection.done(used for handlers which should only be wired up after connection)

---
+++ RPGMaster/RPGMaster/RpgHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace RPGMaster
+{
+    public class RpgHub : Hub
+    {
+
+        public void SendMessage(string name, string message)
+        {
+            var user = Clients.Caller.user;
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
