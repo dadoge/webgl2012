19c76c946647907759dfaff330b01320dd30978c
antoncmorgan <antoncmorgan@gmail.com>
2014-10-29 03:20:47 +0000

    -Progress character tab utility by adding inventory and the ability to manage inventory and create new items -Modified hidemenu to finally collapse character-tab to the right, will be modified to expand inner screen along with it -made CSS changes to improve front end

--- RPGMaster/RPGMaster/Scripts/Site/BabylonScene.js
+++ RPGMaster/RPGMaster/Scripts/Site/BabylonScene.js
@@ -7,45 +7,53 @@
     var light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 100, 100), scene);
 
     //Adding an Arc Rotate Camera
+    //var camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 1, -15), scene);
     var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 3, 1.2, 30, new BABYLON.Vector3.Zero(), scene);
+    
+    scene.tileMaterialFloor = new BABYLON.StandardMaterial("tile-texture-Floor", scene);
+    scene.tileMaterialFloor.diffuseColor = new BABYLON.Color3(0.6, 0.6, 0.58);
 
+    scene.tileMaterialWall = new BABYLON.StandardMaterial("tile-texture-Wall", scene);
+    scene.tileMaterialWall.diffuseColor = new BABYLON.Color3(0.2, 0.2, 0.18);
 
-    //var freeCamera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 0, -30), scene);
-
-    //var box = BABYLON.Mesh.CreateBox("Box", 6.0, scene);
-
-    //scene.enablePhysics();
-    //scene.setGravity(new BABYLON.Vector3(0, -9.81, 0));
-
-    var tileMaterialFloor = new BABYLON.StandardMaterial("texture1", scene);
-    tileMaterialFloor.diffuseColor = new BABYLON.Color3(0.6, 0.6, 0.58);
-
-    var tileMaterialWall = new BABYLON.StandardMaterial("texture1", scene);
-    tileMaterialWall.diffuseColor = new BABYLON.Color3(0.2, 0.2, 0.18);
-
-    var tile = [];
-    var tileZ = 0; var tileX = -4 * 2.7;
-    zOffset = -4 * 2.7;
-    for (var i = 0; i < 100; i++) {
-        if (tileZ > 9) {
+    scene.tile = [];
+    scene.updateQueue = [];
+    scene.loadMap = 0;
+    scene.updateQueue.tileId = [];
+    gameStateHelper.gameMap.Tiles = [];
+    var MapHeight = 20;
+    var MapLength = 20
+    var tileZ = 0; var tileX = -(MapHeight/2 - 1) * 2.7;
+    zOffset = -(MapLength / 2 - 1) * 2.7;
+    for (var i = 0; i < (MapHeight*MapLength); i++) {
+        if (tileZ > (MapLength - 1)) {
             tileZ = 0;
             tileX += 2.7;
         }
-        tile[i] = BABYLON.Mesh.CreateCylinder("cylinder", .25, 3, 3, 6, scene, false);
-        tile[i].material = tileMaterialFloor;
-        tile[i].position = new BABYLON.Vector3(tileX - 1.35 * (tileZ % 2), 0, tileZ * 2.35 + zOffset);
+        scene.tile[i] = BABYLON.Mesh.CreateCylinder("tile-" + i, .25, 3, 3, 6, scene, false);
+        scene.tile[i].material = scene.tileMaterialFloor;
+        scene.tile[i].tileId = i;
+        scene.tile[i].position = new BABYLON.Vector3(tileX - 1.35 * (tileZ % 2), 0, tileZ * 2.35 + zOffset);
+        var mapTile = new MapTile();
+        mapTile.insertTileData(scene.tile[i]);
+        gameStateHelper.gameMap.Name = "Map-00";
+        gameStateHelper.gameMap.Tiles.push(mapTile);
         tileZ++;
     }
 
+    //BABYLON.SceneLoader.ImportMesh("", "../BabylonModels/", "golem_clean.babylon", scene, function (meshes) {
+    //    var m = meshes[0];
+    //    m.isVisible = true;
+    //    //m.scaling = new BABYLON.Vector3(20, 20, 20);
+    //    m.position = new BABYLON.Vector3(0, 0, 0);
+    //    WOLF_MODEL = m;
+    //});
+    //var newMeshes;
+    //BABYLON.SceneLoader.ImportMesh("", "../BabylonModels/", "Crow-rev001.babylon", scene, function (NewMeshes) {
+    //    this.newMeshes = NewMeshes;
+    //});
 
-
-    //scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
-    //scene.collisionsEnabled = true;
-    //box.checkCollisions = true;
-    //plan.checkCollisions = true;
-
-    //box.applyGravity = true;
-
+    //var test = newMeshes;
 
     scene.registerBeforeRender(function () {
         //if (box.intersectsMesh(plan, true)) {
@@ -56,35 +64,23 @@
     });
 
     //When click event is raised
-    window.addEventListener("click", function (evt) {
+    //document.getElementById('interactive-inner').removeEventListener("click",arguments.callee,false);
+    document.getElementById('interactive-inner').addEventListener("click", function (evt) {
         //alert("Screen Width: " + screen.width + "\nScreen Height: " + screen.height + "\nInner Width: " + window.innerWidth + "\nOuter Width: " + window.outerWidth + "\n\nClick X: " + evt.clientX + "\nClick Y: " + evt.clientY);
         //alert("Click X: " + evt.clientX + "\nClick Y: " + evt.clientY + "\n\nOffset X: " + evt.offsetX + "\nOffset Y: " + evt.offsetY + "\n\nTotal X: " + (evt.clientX + evt.offsetX) + "\nTotal Y: " + (evt.clientY + evt.offsetY));
         var scale = getScale();
         if (scale == null) {
             scale = 1;
         }
-        $.sendMessage('jel', 'jel');
         // We try to pick an object
         //evt.clientY*scale -
         //evt.clientX*scale -
         var pickResult = scene.pick(evt.offsetX, evt.offsetY);
 
-        var test = pickResult.pickedMesh;
-
         // if the click hits the ground object, we change the impact position
         if (pickResult.hit) {
-            if (pickResult.pickedMesh.scaling.y == 1) {
-                pickResult.pickedMesh.scaling.y = 10;
-                pickResult.pickedMesh.position.y += pickResult.pickedMesh.scaling.y * .125;
-                pickResult.pickedMesh.material = tileMaterialWall;
-            }
-            else {
-                pickResult.pickedMesh.position.y -= pickResult.pickedMesh.scaling.y * .125;
-                pickResult.pickedMesh.scaling.y = 1;
-                pickResult.pickedMesh.material = tileMaterialFloor;
-            }
+            $.updateTile(pickResult.pickedMesh.tileId);
         }
-
     });
 
     return scene;
--- RPGMaster/RPGMaster/Scripts/Site/hidemenu.js
+++ RPGMaster/RPGMaster/Scripts/Site/hidemenu.js
@@ -13,13 +13,23 @@
 $("#character-hide").click(function () {
     if (document.getElementById('character-display').style.borderStyle == "solid") {
         document.getElementById('character-display').style.borderStyle = "none";
-        $("#character-display").animate({ width: "toggle" }, 1000);
+        $('#options-character-info').show();
+        $('.character-tab').show();
+        $('.character-info').css({ padding: "5px 5px 0px 20px" });
+        $("#character-display").animate({ width: '25%' }, 800, function () {
+            $('.character-icon-footer').fadeIn(200);
+        });
         //$("#character-display").animate({ "left": "+100px" }, "slow").removeClass('visible');
       //  $("#character-display").hide({ direction: "right" }, "slow");
     }
     else {
         document.getElementById('character-display').style.borderStyle = "solid";
-        $("#character-display").animate({ width: "toggle" }, 1000);
+        $('.character-icon-footer').fadeOut(400);
+        $('#options-character-info').fadeOut(400, function () {
+            $('.character-info').css({ padding: "5px 5px 0px 5px" });
+        });
+        $('.character-tab').fadeOut(400);
+        $("#character-display").animate({ width: "25px",right:0 }, 800);
         //$("#character-display").animate({"left":"0px"}, "slow").addClass('visible');
         //document.getElementById('#menu-input').focus();
     }
